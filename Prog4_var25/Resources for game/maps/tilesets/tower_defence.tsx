c9eac778898e54df825f1ebf4edf8d0036ab09e5
Svalkash <zhenya1234pot@yandex.ru>
2022-03-05 01:29:41 +0300

    resourcesfor425

---
+++ Prog4_var25/Resources for game/maps/tilesets/tower_defence.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="tower_defence" tilewidth="16" tileheight="16" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="sprites/grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="sprites/water.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="sprites/hill.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="sprites/wall.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="sprites/tower.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="sprites/castle.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="sprites/spawn.png"/>
+ </tile>
+</tileset>
